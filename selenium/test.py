9e571b46290ee09e0e01b182ae099b2c8b5b907e
Sudheer Bhat <sudheer.k.bhat@gmail.com>
2021-06-12 15:04:21 +0530

    selenium WIP2

--- selenium/test.py
+++ selenium/test.py
@@ -1,14 +1,16 @@
 from selenium.webdriver import Chrome
 
-browser = Chrome('/usr/local/bin/chromedriver')
-browser.get('https://duckduckgo.com')
+class TestSuite:
 
-search_form = browser.find_element_by_id('search_form_input_homepage')
-search_form.send_keys('real python')
-search_form.submit()
+    def test_one(self):
+        browser = Chrome('/usr/local/bin/chromedriver')
+        browser.get('https://duckduckgo.com')
 
-results = browser.find_elements_by_class_name('result')
-print(results[0].text)
+        search_form = browser.find_element_by_id('search_form_input_homepage')
+        search_form.send_keys('real python')
+        search_form.submit()
 
-browser.close()
-quit()
+        results = browser.find_elements_by_class_name('result')
+        assert 'https://realpython.com' in results[0].text
+
+        browser.close()

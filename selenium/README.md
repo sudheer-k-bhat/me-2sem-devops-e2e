3d77411029ba78693bd7e9d65856f62b2785735c
Sudheer Bhat <sudheer.k.bhat@gmail.com>
2021-06-12 14:28:49 +0530

    Selenium work-in-progress.

---
+++ selenium/README.md
@@ -0,0 +1,7 @@
+1. Install Chrome webdriver from https://chromedriver.storage.googleapis.com/91.0.4472.101/chromedriver_mac64.zip, unzip & place in PATH.
+
+# Jenkins on macOS
+```
+brew install jenkins-lts
+brew services start jenkins-lts
+```
---
+++ selenium/test.py
@@ -0,0 +1,14 @@
+from selenium.webdriver import Chrome
+
+browser = Chrome('chromedriver')
+browser.get('https://duckduckgo.com')
+
+search_form = browser.find_element_by_id('search_form_input_homepage')
+search_form.send_keys('real python')
+search_form.submit()
+
+results = browser.find_elements_by_class_name('result')
+print(results[0].text)
+
+browser.close()
+quit()
